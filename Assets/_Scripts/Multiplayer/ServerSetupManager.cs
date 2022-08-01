6c0898bcbc750bfb0103a96991dde867b5792cc4
Gökay <ygokayo@hotmail.com>
2022-08-01 23:50:07 +0300

    added server setup manager

--- Assets/_Scripts/Multiplayer/ServerSetupManager.cs
+++ Assets/_Scripts/Multiplayer/ServerSetupManager.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using System.IO;
 
 public class ServerSetupManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        if (!PhotonNetwork.IsMasterClient) return;
+        SetupServer();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetupServer()
     {
-        
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Cube"), new Vector3(0,0,10), Quaternion.identity);
     }
 }
